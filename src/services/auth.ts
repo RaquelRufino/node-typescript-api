9d7550167e413cc56bb377830432f52cbb837305
RaquelRufino <quelrufino35@gmail.com>
2021-05-25 14:03:26 -0300

    Part 4

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,7 @@
+
 import bcrypt from 'bcrypt';
+import config from 'config';
+import jwt from 'jsonwebtoken';
 
 export default class AuthService {
     public static async hashPassword(
@@ -13,5 +16,11 @@ export default class AuthService {
         hashedPassword: string
     ): Promise<boolean> {
         return await bcrypt.compare(password, hashedPassword);
+    }
+
+    public static generateToken(payload: object): string {
+        return jwt.sign(payload, config.get('App.auth.key'), {
+            expiresIn: config.get('App.auth.tokenExpiresIn'),
+        });
     }
 }
